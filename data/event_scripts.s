d1b5da898056353c779d78c2e194d7dff9b5fcd9
Seth Barberee <seth.barberee@gmail.com>
2020-07-14 16:46:28 -0500

    add Wolfe Nursery and Shop

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1060,3 +1060,7 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/WolfeNursery/scripts.inc"
+
+	.include "data/maps/WolfeNurseryShop/scripts.inc"
