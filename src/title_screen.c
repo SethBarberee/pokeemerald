b5245daf646fffabcb645fcbeb9c2346afbbe529
Seth Barberee <seth.barberee@gmail.com>
2020-08-06 19:20:16 -0500

    disable the scroll on BG1 since it's empty now

--- src/title_screen.c
+++ src/title_screen.c
@@ -755,13 +755,13 @@ static void Task_TitleScreenPhase3(u8 taskId)
         SetGpuReg(REG_OFFSET_BG2Y_L, 0);
         SetGpuReg(REG_OFFSET_BG2Y_H, 0);
         gTasks[taskId].tCounter++;
-        // TODO maybe disable background scroll
-        if (gTasks[taskId].tCounter & 1)
-        {
-            gTasks[taskId].data[4]++;
-            gBattle_BG1_Y = gTasks[taskId].data[4] / 2;
-            gBattle_BG1_X = 0;
-        }
+        // disable background scroll on BG1 (nothing on it anyway)
+        //if (gTasks[taskId].tCounter & 1)
+        //{
+        //    gTasks[taskId].data[4]++;
+        //    gBattle_BG1_Y = gTasks[taskId].data[4] / 2;
+        //    gBattle_BG1_X = 0;
+        //}
         UpdateLegendaryMarkingColor(gTasks[taskId].tCounter);
         if ((gMPlayInfo_BGM.status & 0xFFFF) == 0)
         {
